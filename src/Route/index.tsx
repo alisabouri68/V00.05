44f6c8881efc5f7638ac10619e7ef5cf5c96515f
aliEmami <alisabouri6857@gmail.com>
2026-07-18 15:51:30 +0330

    test

--- src/Route/index.tsx
+++ src/Route/index.tsx
@@ -37,7 +37,7 @@ const routes: RouteObject[] = [
         ),
       },
       {
-        path: ":bundleName/:serviceName/:sheetName?/:id?",
+        path: ":bundleName?/:serviceName?/:sheetName?/:id?",
         Component: () => (
           <AuthProvider>
             <ErrorBoundary>
